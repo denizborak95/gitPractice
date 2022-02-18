3bef606bdbd1769bcfbe71f65aa9db394cb09ecc
denizborak95 <borakdnz@gmail.com>
2022-02-17 21:52:43 -0800

    git is easier

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -6,7 +6,7 @@ public class batch12 {
 		System.out.println("that's what she said");
 		System.out.println("hello world");
 		System.out.println("hello hello");
-		
+		System.out.println("hello worlddddd");
 		
 	}
 
