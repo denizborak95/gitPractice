fc37a5bcdd49ba64f1aca1d41fa06029db3c855a
denizborak95 <borakdnz@gmail.com>
2022-02-17 21:48:28 -0800

    the commit that was easy

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,7 +5,7 @@ public class batch12 {
 	public static void main(String[] args) {
 		System.out.println("that's what she said");
 		System.out.println("hello world");
-		
+		System.out.println("hello hello");
 		
 		
 	}
